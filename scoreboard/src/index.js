1fe95daa1658619d9041576dd1ee94957c251740
Piotr <prusinski.pio@gmail.com>
2018-07-12 13:43:26 +0200

    Implement reset button functionality, finish scoreboard

--- scoreboard/src/index.js
+++ scoreboard/src/index.js
@@ -25,11 +25,35 @@ var Stopwatch = React.createClass({
   getInitialState: function() {
     return {
       running: false,
+      elapsedTime: 0,
+      previousTime: 0,
     }
   },
 
+  componentDidMount: function(){
+    this.interval = setInterval(this.onTick, 100);
+  },
+
+  componentWillUnmount: function(){
+    clearInterval(this.interval);
+  },
+
+  onTick: function(){
+    if (this.state.running) {
+      var now = Date.now()
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      })
+    }
+    console.log('onTick')
+  },
+
   onStart: function(){
-    this.setState({ running: true });
+    this.setState({ 
+      running: true, 
+      previousTime: Date.now()
+    });
   },
 
   onStop: function(){
@@ -37,14 +61,18 @@ var Stopwatch = React.createClass({
   },
 
   onReset: function(){
-
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    });
   },
 
   render: function(){
+    var seconds = Math.floor(this.state.elapsedTime / 1000);
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <div className="stopwatch-time">0</div>
+        <div className="stopwatch-time">{ seconds }</div>
         { this.state.running ? 
             <button onClick={this.onStop}>Stop</button> 
             :
