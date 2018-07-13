ff7c3ed4c2fc31a98a391bb602f96b5eb0eec563
Piotr <prusinski.pio@gmail.com>
2018-07-13 12:50:50 +0200

    implement filtering unconfirmed guests

--- rsvp/src/GuestList.js
+++ rsvp/src/GuestList.js
@@ -5,10 +5,12 @@ import Guest from './Guest';
 
 const GuestList = props => 
 <ul>
-    {props.guests.map((guests, index) =>
+    {props.guests
+        .filter(guest => !props.isFiltered || guest.isConfirmed)
+        .map((guests, index) =>
         <Guest 
             key={index} 
-            name={guests.name} 
+            name={guests.name}
             isConfirmed={guests.isConfirmed}
             isEditing={guests.isEditing}
             handleConfirmation={() => props.toggleConfirmationAt(index)}
@@ -23,6 +25,7 @@ GuestList.propTypes = {
     toggleConfirmationAt: PropTypes.func.isRequired,
     toggleEditingAt: PropTypes.func.isRequired,
     setNameAt: PropTypes.func.isRequired,
+    isFiltered: PropTypes.bool.isRequired,
 }
 
 export default GuestList;
