96bcbc02a96c2d584871c56db24386ae432db684
Piotr <prusinski.pio@gmail.com>
2018-07-13 16:16:18 +0200

    show pending user on form input

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -140,6 +140,7 @@ class App extends Component {
           removeGuestAt={this.removeGuestAt}
           setNameAt={this.setNameAt}
           isFiltered={this.state.isFiltered}
+          pendingGuest={this.state.pendingGuest}
         />
 
       </div>
